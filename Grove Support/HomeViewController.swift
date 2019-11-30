410591767db7ad6fcbbcb0c6f2ef7cdee3278b51
Jon Brown <jon@grovetech.co>
2019-11-29 23:54:34 -0500

    fixed grid view.

--- Grove Support/HomeViewController.swift
+++ Grove Support/HomeViewController.swift
@@ -184,7 +184,7 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 10, bottom: 10, right: 10)
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
     
     override var prefersStatusBarHidden: Bool {
