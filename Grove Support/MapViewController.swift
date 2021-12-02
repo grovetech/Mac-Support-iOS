5e45ed0aa1f2145c302f3e9ace767d5af9666629
Jon Brown <jon@grovetech.co>
2021-12-01 22:32:11 -0500

    Added locations to Map

--- Grove Support/MapViewController.swift
+++ Grove Support/MapViewController.swift
@@ -38,6 +38,12 @@ class mapViewController: UIViewController {
         mapView.delegate = self
         mapView.register(CustomAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         
+        
+        
+        
+        let mapcenter = CLLocationCoordinate2D(latitude: 38.822687,
+                                              longitude: -101.622293)
+        
         let location = CLLocationCoordinate2D(latitude: 38.903442,
                                               longitude: -77.0349633)
         
@@ -47,29 +53,79 @@ class mapViewController: UIViewController {
         let location3 = CLLocationCoordinate2D(latitude: 39.0402,
                                                longitude: -76.998328)
         
+        let location4 = CLLocationCoordinate2D(latitude: 32.8327132,
+                                               longitude: -83.6792745)
+        
+        let location5 = CLLocationCoordinate2D(latitude: 34.398582,
+                                               longitude: -119.7849607)
+        
+        let location6 = CLLocationCoordinate2D(latitude:  32.5712265,
+                                               longitude: -87.80447)
+       
+        let location7 = CLLocationCoordinate2D(latitude:  27.459647,
+                                               longitude: -81.356423)
+        
+        let location8 = CLLocationCoordinate2D(latitude:  43.7182197,
+                                               longitude: -79.4482688)
+        
+        
         // 2
-        let span = MKCoordinateSpan(latitudeDelta: 0.55, longitudeDelta: 0.55)
-        let region = MKCoordinateRegion(center: location, span: span)
+        let span = MKCoordinateSpan(latitudeDelta: 30.95, longitudeDelta: 45.95)
+        let region = MKCoordinateRegion(center: mapcenter, span: span)
         mapView.setRegion(region, animated: true)
         
         //3
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
         annotation.title = "Grove Technologies"
-        annotation.subtitle = "1030 15th St NW Suite 1050w, Washington, DC 20005"
+        annotation.subtitle = "Nationwide Fully Remote & Distributed"
         mapView.addAnnotation(annotation)
         
         let annotation2 = MKPointAnnotation()
         annotation2.coordinate = location2
         annotation2.title = "Grove Technologies"
-        annotation2.subtitle = "Serving the DC, MD and VA Regions"
+        annotation2.subtitle = "Nationwide Fully Remote & Distributed"
         mapView.addAnnotation(annotation2)
         
         let annotation3 = MKPointAnnotation()
         annotation3.coordinate = location3
         annotation3.title = "Grove Technologies"
-        annotation3.subtitle = "Serving the DC, MD and VA Regions"
+        annotation3.subtitle = "Nationwide Fully Remote & Distributed"
         mapView.addAnnotation(annotation3)
+        
+        
+        let annotation4 = MKPointAnnotation()
+        annotation4.coordinate = location4
+        annotation4.title = "Grove Technologies"
+        annotation4.subtitle = "Nationwide Fully Remote & Distributed"
+        mapView.addAnnotation(annotation4)
+        
+        let annotation5 = MKPointAnnotation()
+        annotation5.coordinate = location5
+        annotation5.title = "Grove Technologies"
+        annotation5.subtitle = "Nationwide Fully Remote & Distributed"
+        mapView.addAnnotation(annotation5)
+        
+        let annotation6 = MKPointAnnotation()
+        annotation6.coordinate = location6
+        annotation6.title = "Grove Technologies"
+        annotation6.subtitle = "Nationwide Fully Remote & Distributed"
+        mapView.addAnnotation(annotation6)
+        
+        
+        let annotation7 = MKPointAnnotation()
+        annotation7.coordinate = location7
+        annotation7.title = "Grove Technologies"
+        annotation7.subtitle = "Nationwide Fully Remote & Distributed"
+        mapView.addAnnotation(annotation7)
+        
+        let annotation8 = MKPointAnnotation()
+        annotation8.coordinate = location8
+        annotation8.title = "Grove Technologies"
+        annotation8.subtitle = "Nationwide Fully Remote & Distributed"
+        mapView.addAnnotation(annotation8)
+        
+        
         
         let yourAnnotationAtIndex = 0
         mapView.selectAnnotation(mapView.annotations[yourAnnotationAtIndex], animated: true)
